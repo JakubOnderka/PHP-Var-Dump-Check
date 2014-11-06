1860e14f07bc6c246965973f6fa1d5c2c3a7d4b0
Václav Makeš <info@makes.cz>
2014-11-06 11:38:17 +0100

    Tests - added test for Ladybug

---
+++ tests/JakubOnderka/PhpVarDumpCheck/LadybugTest.php
@@ -0,0 +1,65 @@
+<?php
+
+use JakubOnderka\PhpVarDumpCheck;
+
+class LadybugTest extends PHPUnit_Framework_TestCase
+{
+    private $uut;
+
+
+    public function __construct()
+    {
+        $settings = new PhpVarDumpCheck\Settings();
+        $settings->functionsToCheck = array_merge($settings->functionsToCheck, array(
+            PhpVarDumpCheck\Settings::LADYBUG_DUMP,
+            PhpVarDumpCheck\Settings::LADYBUG_DUMP_DIE,
+            PhpVarDumpCheck\Settings::LADYBUG_DUMP_SHORTCUT,
+            PhpVarDumpCheck\Settings::LADYBUG_DUMP_DIE_SHORTCUT,
+        ));
+        $this->uut = new PhpVarDumpCheck\Checker($settings);
+    }
+
+
+    public function testCheck_ladybugDump()
+    {
+        $content = <<<PHP
+<?php
+ladybug_dump(\$var);
+PHP;
+        $result = $this->uut->check($content);
+        $this->assertCount(1, $result);
+    }
+
+
+    public function testCheck_ladybugDumpDie()
+    {
+        $content = <<<PHP
+<?php
+ladybug_dump_die(\$var);
+PHP;
+        $result = $this->uut->check($content);
+        $this->assertCount(1, $result);
+    }
+
+
+    public function testCheck_ladybugDumpShortcut()
+    {
+        $content = <<<PHP
+<?php
+ld(\$var);
+PHP;
+        $result = $this->uut->check($content);
+        $this->assertCount(1, $result);
+    }
+
+
+    public function testCheck_ladybugDumpDieShortcut()
+    {
+        $content = <<<PHP
+<?php
+ldd(\$var);
+PHP;
+        $result = $this->uut->check($content);
+        $this->assertCount(1, $result);
+    }
+}
